6f0791e954ba7363d809b13da5bdfab7fbc4a18a
Ashley Moy <mac@Ashleys-MacBook-Pro.local>
2020-05-10 11:58:04 -0400

    Add env variables

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from classes import Fridge
+import os
+from dotenv import load_dotenv
+
+load_dotenv() # load env variables 
+
+def send_text():
+    # get environment variables
+    acct_id = os.getenv('account_sid')
+    auth_token = os.environ.get('auth_token')
+    print(acct_id)
 
 if __name__ == "__main__":
-    samsung = Fridge('Samsung','RF23R6201WW')
-    print(samsung.check_home_depot())
+    send_text()
+    # samsung = Fridge('Samsung','RF23R6201WW')
+    # print(samsung.check_home_depot())
